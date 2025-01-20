87a70a3389b82633ea9d6891aaa6b99a19485d9d
alex <cavaleralexandru22@stud.ase.ro>
2025-01-20 23:36:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Login from "./Views/Pages/Login/login";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Modificat de la Switch la Routes
-=======
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
->>>>>>> fa51d84dcad7e89172ce15cf4023b93dc017cb7a
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./Views/sidebar/sidebar";
 import ViewBugPage from "./Views/Pages/viewBugs";
 import CreateBug from "./Views/Components/Bug Create/bugForm";
@@ -20,15 +16,8 @@ function App() {
       ) : (
         <>
           <Sidebar />
-<<<<<<< HEAD
-          <Routes> {/* Utilizat Routes în loc de Switch */}
-            <Route path="/" element={<Dashboard />} /> {/* Corect utilizat element */}
-=======
-          <Switch>
-            <Route path="/" exact>
-              <Dashboard />
-            </Route>
->>>>>>> fa51d84dcad7e89172ce15cf4023b93dc017cb7a
+          <Routes> {}
+            <Route path="/" element={<Dashboard />} /> {/* Utilizare corectă a element */}
             <Route path="/viewbugs" element={<ViewBugPage />} />
             <Route
               path="/create"
@@ -38,11 +27,7 @@ function App() {
                 </div>
               }
             />
-<<<<<<< HEAD
           </Routes>
-=======
-          </Switch>
->>>>>>> fa51d84dcad7e89172ce15cf4023b93dc017cb7a
         </>
       )}
     </Router>
